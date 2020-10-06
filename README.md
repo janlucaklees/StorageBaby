820719252864563bd57101c24ae06d01f545e13d
Jan-Luca Klees <email@janlucaklees.de>
2020-10-06 21:53:21 +0200

    Initial commit / samba config
    First commit starting with support for samba. Having a samba config and
    a basic install file with all the software and a stow command that
    installs the samba config.

---
+++ README.md
@@ -0,0 +1,6 @@
+# StorageBaby
+
+Here I plan on creating and building configuration and scripts for my multi-media sever called StorageBaby.
+In the end I hope to have a solid config for keeping my files save, being able to access them through shares
+and streaming and backup my phone here.
+
---
+++ install.sh
@@ -0,0 +1,13 @@
+# Updating packages and installing dependencies for this script
+sudo pacman -Syu --needed --noconfirm base-devel git stow
+
+# installing all kinds of stuff
+yay -S --needed --noconfirm \
+  mergerfs snapraid \
+  samba elasticsearch fscrawler-bin \
+  plex-media-server \
+  rsync
+
+# install configuration
+sudo stow -vv -t / samba
+
