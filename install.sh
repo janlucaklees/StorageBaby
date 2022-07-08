59651bac74a321f84a7db833c4bfb8ffba7bf297
Jan-Luca Klees <email@janlucaklees.de>
2022-07-08 11:43:36 +0000

    Updated setup
    - Use systemd mounts so that do not need to merge fstab files
    - Updated the Install.sh
      - Removed Plex Mediaserver
      - Added code to configure and enable mounts
      - Added missing packages

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
-# Updating packages and installing dependencies for this script
-sudo pacman -Syu --needed --noconfirm base-devel git stow
-
 # installing all kinds of stuff
 yay -S --needed --noconfirm \
-  mergerfs mergerfs-tools-git snapraid \
+  mergerfs fuse mergerfs-tools-git snapraid \
   samba elasticsearch fscrawler-bin \
-  plex-media-server \
-  jellyfin aur/aspnet-runtime-5.0-bin
+  jellyfin aur/aspnet-runtime-5.0-bin \
   syncthing
 
 # install configuration
 sudo stow -vv -t / snapraid
 sudo stow -vv -t / samba
-sudo stow -vv -t / fstab
+
+# Drives
+sudo stow -vv -t / mounts
+sudo systemctl enable --now mnt-data-data1.mount
+sudo systemctl enable --now mnt-data-data2.mount
+sudo systemctl enable --now mnt-parity-parity1.mount
 
 # Enable and start services
-systemctl enable smb nmb plexmediaserver jellyfin syncthing@jlk
-systemctl start  smb nmb plexmediaserver jellyfin syncthing@jlk
-systemctl status smb nmb plexmediaserver jellyfin syncthing@jlk
+sudo systemctl enable --now smb nmb jellyfin syncthing@jlk
 
