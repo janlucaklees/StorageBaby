94994bfb0534292a5acd03bd4f41834d2651f85c
Jan-Luca Klees <email@janlucaklees.de>
2022-07-08 13:35:25 +0000

    Correctly configured systemd mount units

--- install.sh
+++ install.sh
@@ -1,20 +1,33 @@
-# installing all kinds of stuff
+#
+# Installing required packages
 yay -S --needed --noconfirm \
   mergerfs fuse mergerfs-tools-git snapraid \
   samba elasticsearch fscrawler-bin \
   jellyfin aur/aspnet-runtime-5.0-bin \
   syncthing
 
+#
 # install configuration
 sudo stow -vv -t / snapraid
 sudo stow -vv -t / samba
 
+#
 # Drives
+
+# Install configuration
 sudo stow -vv -t / mounts
+
+# Enable and mount data drives
 sudo systemctl enable --now mnt-data-data1.mount
 sudo systemctl enable --now mnt-data-data2.mount
+
+# Enable and mount parity drives
 sudo systemctl enable --now mnt-parity-parity1.mount
 
+# Enable and mount the pool
+sudo systemctl enable --now pool.mount
+
+#
 # Enable and start services
 sudo systemctl enable --now smb nmb jellyfin syncthing@jlk
 
