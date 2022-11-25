bcfd842f21c813509ab02304e342ac3b7ff7e8c9
Jan-Luca Klees <email@janlucaklees.de>
2022-11-25 08:38:21 +0100

    Added ToDos

--- safe-sync.sh
+++ safe-sync.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-systemctl stop smb nmb jellyfin syncthing@jlk
+# TODO: Stop docker services
+systemctl stop smb nmb syncthing@jlk
 
 snapraid touch
 
 snapraid sync
 
-systemctl start smb nmb jellyfin syncthing@jlk
+systemctl start smb nmb syncthing@jlk
+# TODO: Start Docker services again
 
