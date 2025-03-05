0a0e5c96888637f7a08a0f08560d4771f6ec60ed
Jan-Luca Klees <email@janlucaklees.de>
2025-03-05 19:27:56 +0100

    Corrected comment

--- persistence/plugins/paperless/on-after-sync.sh
+++ persistence/plugins/paperless/on-after-sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Start up Jellyfin again as sync is over and it is safe again to change data.
+# Start up Paperless again as sync is over and it is safe again to change data.
 docker start paperless-broker-1 paperless-tika-1 paperless-db-1 paperless-gotenberg-1
 docker start paperless-webserver-1
