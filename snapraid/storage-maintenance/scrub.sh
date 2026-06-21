87036f5866d8c33f10a6762040bf8e20e2258ffe
Jan-Luca Klees <email@janlucaklees.de>
2026-06-21 13:12:45 +0200

    Improved snapraid scrub to scrub both new and exisitng files

--- snapraid/storage-maintenance/scrub.sh
+++ snapraid/storage-maintenance/scrub.sh
@@ -4,4 +4,5 @@
 mkdir -p /var/log/snapraid
 
 # Runs the scrub.
-snapraid scrub -p new --log /var/log/snapraid/snapraid-scrub.log
+snapraid scrub --plan new --log /var/log/snapraid/snapraid-scrub-new.log
+snapraid scrub --plan 8 --older-than 12 --log /var/log/snapraid/snapraid-scrub.log
